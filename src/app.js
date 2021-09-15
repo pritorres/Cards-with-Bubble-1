91a6a4422c356e87c98ea23add330eba4e738dd4
priscila torres reyes <priscyla.torres16@hotmail.com>
2021-09-15 09:51:58 +0000

    first

--- src/app.js
+++ src/app.js
@@ -2,10 +2,36 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.myFuncion = function() {
+  const myInput = document.querySelector(".myInput");
+  const myPinta = ["diamond", "heart", "club", "spade"];
+  const myNumeroCard = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const cartasSort = document.querySelector(".cartasSort");
+  let myCard = "";
+  for (let index = 0; index < myInput.value; index++) {
+    const pintaAleatoria = Math.floor(Math.random(0) * 4);
+    const numeroAleatorio = Math.floor(Math.random(0) * 13);
+    myCard += `
+      <div class="Cards">
+        <div class="icono-top ${myPinta[pintaAleatoria]}"></div>
+        <div class="numeroCard ">${myNumeroCard[numeroAleatorio]}</div>
+        <div class="icono-bottom ${myPinta[pintaAleatoria]}"></div>
+      </div>
+    `;
+  }
+  cartasSort.innerHTML = myCard;
 };
